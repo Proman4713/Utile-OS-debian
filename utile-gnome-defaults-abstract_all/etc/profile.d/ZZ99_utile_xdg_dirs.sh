cc760d92fc5c6f65b151e481e8a7cf0e72a36c04
Proman4713 <proman4713@gmail.com>
2026-09-09 17:08:50 +0300

    utile-gnome-defaults

--- utile-gnome-defaults-abstract_all/etc/profile.d/ZZ99_utile_xdg_dirs.sh
+++
@@ -1,6 +0,0 @@
-# /etc/profile.d/ZZ99_utile_xdg_dirs.sh - Prepend /usr/share/utile to $XDG_DATA_DIRS
-
-if [ -n "${XDG_DATA_DIRS##*/usr/share/utile*}" ]; then
-  XDG_DATA_DIRS="/usr/share/utile:$XDG_DATA_DIRS"
-  export XDG_DATA_DIRS
-fi
---
+++ utile-gnome-defaults-abstract_all/etc/profile.d/zz99_utile_xdg_dirs.sh
@@ -0,0 +1,4 @@
+# /etc/profile.d/ZZ99_utile_xdg_dirs.sh - Prepend /usr/share/utile to $XDG_DATA_DIRS
+
+XDG_DATA_DIRS="/usr/share/utile:${XDG_DATA_DIRS#*/usr/share/utile:}"
+export XDG_DATA_DIRS
